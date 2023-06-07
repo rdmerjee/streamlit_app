c938d9df6bf2e4a24d36f18fb92e7e291dd965f1
Rik Mukherjee <rdmerjee@gmail.com>
2023-06-06 17:31:11 -0700

    New commit

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ import streamlit as st
 
 
 st.title('Rik Mukherjee Application')
-st.markdown('this is **the special application for Stats 21**')
-st.markdown('this is *The thing you need*')
+st.markdown('This is **the special application for Stats 21**')
+st.markdown('An application that gives you what you need for the project in Stats 21')
 
 st.sidebar.title('More')
 st.sidebar.markdown('This project was done in Spring 2023 for Stats 21')
 
+st.markdown('I recently watched Spider-Man: Across the Spiderverse, and I was deeply moved by the film. The animation was beautiful, the characters were great, the main character was very vulnerable and easy to root for, and the villain was menacing but you could understand why he did what he did.')
+st.markdwon('Do you agree that this movie is good?')
 
 agree = st.checkbox('I agree')
 
@@ -15,7 +17,7 @@ if agree:
     st.write('Great')
     st.markdown('You have agreed')
 
-agree_sidebar = st.sidebar.checkbox('This app is cool')
+agree_sidebar = st.sidebar.checkbox('If this app is cool, check this box.')
 
 if agree_sidebar:
-    st.sidebar.write('You have agreed')
+    st.sidebar.write('You have agreed with me that this app is cool.')
